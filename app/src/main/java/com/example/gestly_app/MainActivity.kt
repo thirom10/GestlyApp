d2cb1598bdcc1b7116f7f3feb53afa045c1e7927
Thiago Gonzalez <150206271+thirom10@users.noreply.github.com>
2025-05-18 18:20:48 -0300

    prueba de login para pr

--- app/src/main/java/com/example/gestly_app/MainActivity.kt
+++ app/src/main/java/com/example/gestly_app/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             GestlyappTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "login",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "hola soy el $name",
         modifier = modifier
     )
 }
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     GestlyappTheme {
-        Greeting("Android")
+        Greeting("login")
     }
 }
